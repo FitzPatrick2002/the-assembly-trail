ba5200225515037fb74a01300b871cdbadfc405c
unknown <rapala.magdalena.work@gmail.com>
2025-10-22 19:20:28 +0200

    Example C# and masm project

---
+++ WpfApplication/Asm/AsmLibrary.asm
@@ -0,0 +1,49 @@
+.data
+.code
+asmAddTwoDoubles proc
+	vaddpd ymm0, ymm0, ymm1
+	ret
+asmAddTwoDoubles endp
+
+asmWeightedAverage proc
+	; Input: rcx = pointer to array of doubles
+	;        rdx = pointer to array of integers
+
+	; xmm0 - doubles * weights
+	; xmm1 - sum of weights
+	xorpd xmm0, xmm0 ;0
+	xorpd xmm1, xmm1 ;0
+
+	movsd xmm2, qword ptr [rcx] ;first double a[0] 
+	mov eax, [rdx]   ;first weight b[0]
+	cvtsi2sd xmm3, eax ; convert int to double
+	addsd xmm1, xmm3 ; add the weight b[0]
+	mulsd xmm2, xmm3 ; a[0] * b[0]
+	addsd xmm0, xmm2 ; + a[0] * b[0] to the sum
+
+	movsd xmm2, qword ptr [rcx+8] ;second double a[1] 
+	mov eax, [rdx+4]   ;weight b[1]
+	cvtsi2sd xmm3, eax ;convert int to double
+	addsd xmm1, xmm3 ;add the weight b[1] to the sum
+	mulsd xmm2, xmm3 ; a[1] * b[1]
+	addsd xmm0, xmm2 ; + a[1] * b[1] to the sum
+
+	movsd xmm2, qword ptr [rcx+16] ;double a[2] 
+	mov eax, [rdx+8]   ;weight b[2]
+	cvtsi2sd xmm3, eax ; convert int to double
+	addsd xmm1, xmm3 ;add the weight b[2] to the sum
+	mulsd xmm2, xmm3 ; a[2] * b[2]
+	addsd xmm0, xmm2 ; + a[2] * b[2] to the sum
+
+	movsd xmm2, qword ptr [rcx+24] ;double a[3] 
+	mov eax, [rdx+12]   ;weight b[3]
+	cvtsi2sd xmm3, eax ; convert int to double
+	addsd xmm1, xmm3 ;add the weight b[3] to the sum
+	mulsd xmm2, xmm3 ; a[3] * b[3]
+	addsd xmm0, xmm2 ; + a[3] * b[3] to the sum
+
+	divsd xmm0,xmm1 ;divide the sum of weights * double by the sum of weights
+	ret ;return xmm0
+asmWeightedAverage endp
+
+end
---
+++ WpfApplication/WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿        using System.Text;
+        using System.Windows;
+        using System.Windows.Controls;
+        using System.Windows.Data;
+        using System.Windows.Documents;
+        using System.Windows.Input;
+        using System.Windows.Media;
+        using System.Windows.Media.Imaging;
+        using System.Windows.Navigation;
+        using System.Windows.Shapes;
+        using System.Runtime.InteropServices;
+
+
+
+        namespace WpfApplication
+        {
+            /// <summary>
+            /// Interaction logic for MainWindow.xaml
+            /// </summary>
+            public partial class MainWindow : Window
+            {
+                public MainWindow()
+                {
+                    InitializeComponent();
+                }
+                private void Button_Click(object sender, RoutedEventArgs e)
+                {
+                    AsmProxy asmP = new AsmProxy();
+                    double a, b, r;
+                    a = Double.Parse(txtA.Text);           //convert argument 1 from text (WPF UI) to double
+                    b = Double.Parse(txtB.Text);           //convert argument 2 from text (WPF UI) to double
+                    r = asmP.executeAsmAddTwoDoubles(a, b);//execute assembler code
+                    Result.Text = r.ToString();            //return value to the WPF UI as text
+                }
+
+                private void Weighted_Average(object sender, RoutedEventArgs e)
+                {
+                    AsmProxy asmP = new AsmProxy();
+                    double[] values = new double[4];
+                    int[] weights = new int[4];
+                    double result;
+                    values[0] = Double.Parse(DoubleA.Text);           //convert argument 1 from text (WPF UI) to double
+                    values[1] = Double.Parse(DoubleB.Text);
+                    values[2] = Double.Parse(DoubleC.Text);
+                    values[3] = Double.Parse(DoubleD.Text);
+                    weights[0] = int.Parse(IntA.Text);           //convert argument 2 from text (WPF UI) to double
+                    weights[1] = int.Parse(IntB.Text);
+                    weights[2] = int.Parse(IntC.Text);
+                    weights[3] = int.Parse(IntD.Text);
+                    result = asmP.executeAsmWeightedAverage(values, weights);//execute assembler code
+                    ResultWeight.Text = result.ToString();            //return value to the WPF UI as text
+                }
+            }
+            public unsafe class AsmProxy
+            {
+                [DllImport("Asm.dll")]
+                private static extern double asmAddTwoDoubles(double a, double b);
+
+                [DllImport("Asm.dll")]
+                private static extern unsafe double asmWeightedAverage(double* a, int* b);
+                public double executeAsmAddTwoDoubles(double a, double b)
+                {
+                    return asmAddTwoDoubles(a, b);
+                }
+                public double executeAsmWeightedAverage(double[] a, int[] b)
+                {
+                    fixed (double* ptrA = a)
+                    fixed (int* ptrB = b)
+                    {
+                        return asmWeightedAverage(ptrA, ptrB);
+                    }
+             
+                }
+            }
+
+
+        }
