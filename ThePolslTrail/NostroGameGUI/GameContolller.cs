61a438e500c179007ce211e05a288b186de2e5ae
ruthzhou <ruthmzhou@gmail.com>
2026-01-14 12:29:25 +0100

    initial GUI with resources from Danill

---
+++ ThePolslTrail/NostroGameGUI/GameContolller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NostroGameGUI
+{
+        public class GameController
+        {
+            private int step = 0;
+
+            public string Start()
+            {
+                return "Welcome to The Polsl Trail...\n\nDo you want to continue?";
+            }
+
+            public string HandleYes()
+            {
+                step++;
+
+                if (step == 1)
+                    return "You chose YES.\n\nYou enter the dark corridor.";
+
+                return "YES has no effect right now.";
+            }
+
+            public string HandleNo()
+            {
+                return "You chose NO.\n\nGame over.";
+            }
+        }
+    }
